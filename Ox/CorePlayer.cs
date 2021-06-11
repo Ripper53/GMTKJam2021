07e78a114a1740313d646aa7731a409b90a1a9f0
InFormalOx <84478228+InFormalOx@users.noreply.github.com>
2021-06-12 00:22:27 +0200

    Add files via upload
    Player movement scripts done. Implementing the ball.

--- Ox/CorePlayer.cs
+++ Ox/CorePlayer.cs
@@ -1,26 +1,26 @@
 ﻿using UnityEngine;
+using System;
 
 public class CorePlayer : MonoBehaviour {
 	private CoreMovement    coreM;
 
 	private Vector3     deltaLastInput;
+	private Vector3Int  deltaRaises;
 
-	static float        inputDeadzone;
+	static float        inputDeadzone = 0.05f;
+
+	private int RaiseFunction ( float a1, float a2 ) {
+		if ( Math.Abs ( a1 ) > Math.Abs ( a2 ) ) { return 1 * Math.Sign( a1 ); }
+		
+		if ( Math.Abs ( a1 - a2 ) < inputDeadzone ) { return ( int ) Math.Round( a1, 0 ); }
+	
+		return -1;
+	}
 
 	public void InputTick ( Vector3 a1 ) {
-		Vector3Int deltaRaises = Vector3Int.zero;
-		deltaRaises.x = -1;
-		if ( Modulus ( a1.x ) > Modulus ( deltaLastInput.x ) ) { deltaRaises.x = 1; } else {
-			if ( Modulus ( a1.x - deltaLastInput.x ) < inputDeadzone ) { deltaRaises.x = 0; }
-		}
-		deltaRaises.y = -1;
-		if ( Modulus ( a1.y ) > Modulus ( deltaLastInput.y ) ) { deltaRaises.y = 1; } else {
-			if ( Modulus ( a1.y - deltaLastInput.y ) < inputDeadzone ) { deltaRaises.y = 0; }
-		}
-		deltaRaises.z = -1;
-		if ( Modulus ( a1.z ) > Modulus ( deltaLastInput.z ) ) { deltaRaises.z = 1; } else {
-			if ( Modulus ( a1.z - deltaLastInput.z ) < inputDeadzone ) { deltaRaises.z = 0; }
-		}
+		deltaRaises.x = RaiseFunction ( a1.x, deltaLastInput.x );
+		deltaRaises.y = RaiseFunction ( a1.y, deltaLastInput.y );
+		deltaRaises.z = RaiseFunction ( a1.z, deltaLastInput.z );
 		deltaLastInput = a1;
 	}
 
@@ -34,7 +34,11 @@ public class CorePlayer : MonoBehaviour {
 		delta.y = Input.GetAxis ( "Jump" );
 		delta.z = Input.GetAxis ( "Vertical" );
 
-		coreM.InputTick ( delta );
+		InputTick ( delta );
+
+		delta = Vector3.zero;
+
+		coreM.InputTick ( deltaRaises );
 	}
 
 	float Modulus ( float a1 ) {
--- Ox/CoreView.cs
+++ Ox/CoreView.cs
@@ -1,18 +1,41 @@
 ﻿using UnityEngine;
 
-public class View : MonoBehaviour {
-    public    Transform     rootCore;
+public class CoreView : MonoBehaviour {
+    public    Transform     target;
         private     Transform       rootX;
         private     Transform       rootZ;
 
+    public  Rigidbody       targetRb;
+
     void Start () {
-        if( rootCore != null ) {
-            rootZ = rootCore.GetChild ( 0 );
+        if( target != null ) {
+            rootZ = target.GetChild ( 0 );
             rootX = rootZ.GetChild ( 0 );
         }    
     }
 
-    void Update () {
+	public  Vector2     aSensitivity;
+	public  Vector2     aStrength;
 
-    }
+	private  Vector2     d1;
+	private  Vector2     d2;
+	
+	void Update () {
+		d1.y += Input.GetAxis("Mouse X") * aSensitivity.y;
+		d1.x += Input.GetAxis("Mouse Y") * aSensitivity.x;
+
+		d2.y += d1.y * aStrength.y;
+		d2.x -= d1.x * aStrength.x;
+		
+		if ( d2.x > 90 ) { d1.x -= ( 90 - d2.x ) / aStrength.x; d2.x = 90; }
+		if ( d2.x < -90 ) { d1.x -= ( -90 - d2.x ) / aStrength.x; d2.x = -90; }
+
+		target.transform.Rotate ( 0, d1.y * aStrength.y, 0 );	d1.y *= 1 - aStrength.y;
+		rootX.transform.Rotate ( -d1.x * aStrength.x, 0, 0 );	d1.x *= 1 - aStrength.x;
+	}
+
+	private void FixedUpdate () {
+		targetRb.transform.rotation = target.rotation;
+	}
 }
+
